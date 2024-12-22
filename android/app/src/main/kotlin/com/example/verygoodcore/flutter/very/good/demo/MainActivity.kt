89647f16b481d5877e3672d6e90bd8410c1de536
Calvin Ren <j.calvin.ren@gmail.com>
2024-12-22 16:25:51 +1000

    create very good project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/flutter/very/good/demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flutter_very_good_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_very_good_demo/app/app.dart';
+import 'package:flutter_very_good_demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
